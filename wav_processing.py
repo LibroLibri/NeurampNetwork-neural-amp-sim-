844f1980811e2113c61833472b7d27ee2f67811c
LibroLibri <libro.librizzi@gmail.com>
2020-07-26 16:12:38 -0300

    array to wav

--- wav_processing.py
+++ wav_processing.py
@@ -10,21 +10,20 @@ WAV_format_ranges = {
 }
 
 
-# TODO: remove right channel if stereo
+
 # TODO: resample if not 44.1 KHz
 
-
-def convert_to_array(filename, target_datatype, target_samplerate):
+def convert_to_array(filename, target_datatype='float32', target_samplerate=44100):
     # Read file
     samplerate, data = wavfile.read(filename)
     source_range = WAV_format_ranges[data.dtype]
     # Check that the file format is valid
     if data.dtype not in WAV_format_ranges:
         raise InputError(filename, 'File Format not supported. Try with 8, 16, 32 or 32-Float formats')
-    if samplerate != 44100:
+    if samplerate != target_samplerate:
         raise InputError(filename, 'Samplerate not supported. Use files with 44.1 KHz samplerate')
     # Stereo to Mono
-    if data.shape[1] != 1:
+    if len(data.shape) != 1:
         print('Converting Stereo to Mono')
         data = data.sum(axis=1) / 1.5
     # Take file to float32 format
@@ -36,9 +35,16 @@ def convert_to_array(filename, target_datatype, target_samplerate):
     
     
 
-def convert_to_wav(filename, data, target_datatype):
-    
-    pass
+def convert_to_wav(filename, data, samplerate=44100, target_datatype='float32', source_range=[-1.0, 1.0]):
+    # Take data to target range and datatype
+    if WAV_format_ranges[target_datatype] != source_range:
+        data = np.interp(data, source_range, WAV_format_ranges[np.dtype(target_datatype)])
+    data = data.astype(target_datatype)
+    # Write file and return
+    wavfile.write(filename, samplerate, data)
+    return data
+
+
 class InputError(Exception):
     """Exception raised for errors in the input.
 
